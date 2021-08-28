b056cab91bd2201c32df0ea7fb03a11d76d6d8e0
miguelbj <miguelbjohnson@gmail.com>
2021-08-28 00:14:57 -0400

    Creating higher order functions

--- src/public/client.js
+++ src/public/client.js
@@ -17,30 +17,55 @@ const render = async(root, state) => {
     
 }
 
+function generateDivs(){
+    return `
+    <div class="container">
+        <div id='Curiosity' onclick="onClick('Curiosity')" class="card card0">
+            <div class="border">
+                <h2>Curiosity</h2>
+            </div>
+        </div>
+        <div id='Opportunity' onclick="onClick('Opportunity')" class="card card1">
+            <div class="border">
+                <h2>Opportunity</h2>
+            </div>
+        </div>
+        <div id='Spirit' onclick="onClick('Spirit')"class="card card2">
+            <div class="border">
+                <h2>Spirit</h2>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+
 const App = () => {
     return `
         <main>
-            <div class="container">
-                <div id='Curiosity' onclick="onClick('Curiosity')" class="card card0">
-                    <div class="border">
-                        <h2>Curiosity</h2>
-                    </div>
-                </div>
-                <div id='Opportunity' onclick="onClick('Opportunity')" class="card card1">
-                    <div class="border">
-                        <h2>Opportunity</h2>
-                    </div>
-                </div>
-                <div id='Spirit' onclick="onClick('Spirit')"class="card card2">
-                    <div class="border">
-                        <h2>Spirit</h2>
-                    </div>
-                </div>
-            </div>
+            ${generateDivs()}
         </main>
         <footer></footer>
     `
 }
+
+function cardStyling(roverName){
+    document.getElementById(roverName).innerHTML = `
+    <div><h2 class="card-title" style="color: white">Rover: ${store[roverName].rover.name}</h2></div>
+    <section id:'roverInfoSection'>
+        <p style="color: white">Landing Date: ${store[roverName].rover.landing_date}</p>
+        <p style="color: white">Launch Date: ${store[roverName].rover.launch_date}</p>
+        <p style="color: white">Rover Status: ${store[roverName].rover.status}</p>
+    </section>
+    <div style="color: white">Latest Photo: </p><img src="${store[roverName].img_src}" alt="Latest photo captured by ${roverName} rover" width="500" height="500"/>
+    </div>
+    `
+}
+
+function onClick(roverName) {
+    cardStyling(roverName)
+}
+
 
 roverNames = store.rovers // pull list from the store
 
@@ -51,16 +76,6 @@ roverNames.forEach((roverName) => {
             updateStore(roverName, data.roverPhotos.latest_photos[0]))
 })
 
-function onClick(roverName) {
-    document.getElementById(roverName).innerHTML = `
-    <div><h2 class="card-title" style="color: white">Rover: ${store[roverName].rover.name}</h2></div>
-    <section>
-        <p style="color: white">Landing Date: ${store[roverName].rover.landing_date}</p>
-        <p style="color: white">Launch Date: ${store[roverName].rover.launch_date}</p>
-        <p style="color: white">Rover Status: ${store[roverName].rover.status}</p>
-    </section>
-    <div style="color: white">Latest Photo: </p><img src="${store[roverName].img_src}" alt="Latest photo captured by ${roverName} rover" width="500" height="500"/>
-    </div>
-    `
-}
+
+
 
